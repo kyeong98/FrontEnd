b63a921b83cddc61bbdd9f887b672ab07eb18713
kyeong98 <90947285+kyeong98@users.noreply.github.com>
2023-12-22 17:00:29 +0900

    Create list.js

---
+++ public/js/list.js
@@ -0,0 +1,35 @@
+let boardsStr = localStorage.getItem("boards");
+
+// localStorage 초기값 지정
+if (boardsStr === null) {
+  const listStr = JSON.stringify([]);
+  localStorage.setItem("boards", listStr);
+  boardsStr = listStr;
+}
+
+const boardsObj = JSON.parse(boardsStr);
+
+// 템플릿 생성
+const template = (index, objValue) => {
+  return `
+    <tr>
+        <td>${index + 1}</td>
+        <td><a href="/board/view.html?index=${objValue.index}">${
+    objValue.subject
+  }</a></td>
+        <td>${objValue.writer}</td>
+        <td>${objValue.date}</td>
+        <td>${objValue.views}</td>
+    </tr>
+    `;
+};
+
+// 템플릿 반영
+const tbody = document.querySelector("tbody");
+
+for (let i = 0; i < boardsObj.length; i++) {
+  tbody.innerHTML += template(i, boardsObj[i]);
+  boardsObj[i].refresh = false;
+  const refreshStr = JSON.stringify(boardsObj);
+  localStorage.setItem("boards", refreshStr);
+}
